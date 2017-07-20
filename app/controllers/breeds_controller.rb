ce451f5165f8276058af2bd5b36e4aeeaa8d92f2
Kat Gifford and Steven Galvin <steven@>
2017-07-20 11:57:09 -0700

    add api call/front end

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,4 +1,9 @@
 class BreedsController < ApplicationController
+  before_action do
+    flash[:notice] = "Please log in to see the breeds" unless current_user
+    redirect_to root_path unless current_user
+  end
+
   def index
     @breed = Breed.new
     @cats = @breed.get_cats
--- app/models/breed.rb
+++ app/models/breed.rb
@@ -1,7 +1,7 @@
 class Breed
 
   def get_cats
-    response = RestClient.get('http://localhost:3000/v1/breeds/')
+    response = RestClient.get('http://localhost:3000/v1/breeds?appid=' + ENV['API_KEY'])
     JSON.parse(response)
   end
 end
